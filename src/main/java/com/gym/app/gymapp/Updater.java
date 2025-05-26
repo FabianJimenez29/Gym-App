708a08e69b373aecd1b31f3e639311a6faa59d6a
fabi <fabi@localhost>
2025-05-26 09:49:26 -0600

    Cambios V1.1

--- src/main/java/com/gym/app/gymapp/Updater.java
+++ src/main/java/com/gym/app/gymapp/Updater.java
@@ -9,8 +9,8 @@ public class Updater {
 
     private static final String VERSION_URL = "https://raw.githubusercontent.com/FabianJimenez29/Gym-App/master/version.txt";
     private static final String DOWNLOAD_URL = "https://github.com/FabianJimenez29/Gym-App/releases/download/V1.2/GymApp-V1.2.jar";
-    private static final String NEW_JAR_NAME = "GymApp-V1.2.jar";
-    private static final String VERSION_LOCAL = "V1.0"; // <--- La versión actual del jar ejecutado
+    private static final String NEW_JAR_NAME = "GymApp-V1.1.jar";
+    private static final String VERSION_LOCAL = "V1.1"; // <--- La versión actual del jar ejecutado
 
     public static boolean checkAndUpdate() {
         try {
