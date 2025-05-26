f22a82fb28468e42dbf8b6e0c80bd9b510213cd0
fabi <fabi@localhost>
2025-05-26 08:51:00 -0600

    Cambios Prueba

--- src/main/java/com/gym/app/gymapp/frames/LoginFrameV1.java
+++ src/main/java/com/gym/app/gymapp/frames/LoginFrameV1.java
@@ -53,7 +53,7 @@ public class LoginFrameV1 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(102, 255, 102));
+        jPanel1.setBackground(new java.awt.Color(204, 204, 0));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
